74ea96da0313ab190df45d05afa44b9db2171048
Ramu Pal <ramu.pal2603@gmail.com>
2020-08-24 15:54:51 +0530

    #1 Added Staff SignIn/SignOut From Remote QrCode

---
+++ app/src/main/java/com/brinfotech/feedbacksystem/ui/qrCodeScannerView/QrCodeScannerViewActivity.java
@@ -0,0 +1,123 @@
+package com.brinfotech.feedbacksystem.ui.qrCodeScannerView;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.brinfotech.feedbacksystem.R;
+import com.brinfotech.feedbacksystem.baseClasses.BaseActivity;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class QrCodeScannerViewActivity extends BaseActivity implements View.OnClickListener, ZXingScannerView.ResultHandler, EasyPermissions.PermissionCallbacks,
+        EasyPermissions.RationaleCallbacks {
+
+    @BindView(R.id.qrCodeScannerView)
+    ZXingScannerView qrCodeScanner;
+
+    private static final String[] CAMERA_AND_STORAGE =
+            {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE};
+
+    List<BarcodeFormat> arrFormatList = new ArrayList<>();
+    private static final int MY_CAMERA_REQUEST_CODE = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initializeScannerView();
+
+        startActivityAfterSeconds();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!EasyPermissions.hasPermissions(QrCodeScannerViewActivity.this, CAMERA_AND_STORAGE)) {
+            EasyPermissions.requestPermissions(
+                    this,
+                    getString(R.string.rationale_camera),
+                    MY_CAMERA_REQUEST_CODE,
+                    CAMERA_AND_STORAGE);
+        } else {
+            qrCodeScanner.startCamera();
+            qrCodeScanner.setResultHandler(QrCodeScannerViewActivity.this);
+        }
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void initializeScannerView() {
+        arrFormatList.add(BarcodeFormat.QR_CODE);
+        qrCodeScanner.setFormats(arrFormatList);
+        qrCodeScanner.setAutoFocus(true);
+        qrCodeScanner.setLaserColor(R.color.colorAccent);
+        qrCodeScanner.setMaskColor(R.color.colorAccent);
+
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_scan_qr_code;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        qrCodeScanner.stopCamera();
+    }
+
+    private void callLoginMethod(String userName, String userPassword) {
+        openStaffDashboard();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            qrCodeScanner.startCamera();
+            qrCodeScanner.setResultHandler(QrCodeScannerViewActivity.this);
+        }
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onRationaleAccepted(int requestCode) {
+
+    }
+
+    @Override
+    public void onRationaleDenied(int requestCode) {
+
+    }
+}
--- app/src/main/java/com/brinfotech/feedbacksystem/ui/staffView/dashboard/StaffDashboardActivity.java
+++ app/src/main/java/com/brinfotech/feedbacksystem/ui/staffView/dashboard/StaffDashboardActivity.java
@@ -1,11 +1,31 @@
 package com.brinfotech.feedbacksystem.ui.staffView.dashboard;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.Nullable;
 
 import com.brinfotech.feedbacksystem.R;
 import com.brinfotech.feedbacksystem.baseClasses.BaseActivity;
+import com.brinfotech.feedbacksystem.ui.qrCodeScannerView.QrCodeScannerViewActivity;
+
+import butterknife.BindView;
 
 public class StaffDashboardActivity extends BaseActivity {
+
+
+    @BindView(R.id.rLoutStaffView)
+    RelativeLayout rLoutStaffView;
+
+    int REQUEST_QR_CODE_SCANNER;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        rLoutStaffView.setOnClickListener(this);
+    }
 
     @Override
     protected int getLayoutResource() {
@@ -14,6 +34,14 @@ public class StaffDashboardActivity extends BaseActivity {
 
     @Override
     public void onClick(View view) {
+        if (view == rLoutStaffView) {
+            Intent intent = new Intent(StaffDashboardActivity.this, QrCodeScannerViewActivity.class);
+            startActivityForResult(intent, REQUEST_QR_CODE_SCANNER);
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
