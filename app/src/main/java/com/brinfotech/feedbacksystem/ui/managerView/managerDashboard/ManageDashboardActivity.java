027679e940289cc00574735aefe5087fcb25fda4
Ramu Pal <ramu.pal2603@gmail.com>
2020-08-31 16:46:51 +0530

    1) Added Manager Welcome Text Dynamic.

--- app/src/main/java/com/brinfotech/feedbacksystem/ui/managerView/managerDashboard/ManageDashboardActivity.java
+++ app/src/main/java/com/brinfotech/feedbacksystem/ui/managerView/managerDashboard/ManageDashboardActivity.java
@@ -3,11 +3,14 @@ package com.brinfotech.feedbacksystem.ui.managerView.managerDashboard;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.brinfotech.feedbacksystem.R;
 import com.brinfotech.feedbacksystem.baseClasses.BaseActivity;
+import com.brinfotech.feedbacksystem.helpers.PreferenceKeys;
+import com.pixplicity.easyprefs.library.Prefs;
 
 import butterknife.BindView;
 
@@ -18,11 +21,16 @@ public class ManageDashboardActivity extends BaseActivity {
     @BindView(R.id.rLoutFireEvacuationView)
     RelativeLayout rLoutFireEvacuationView;
 
+    @BindView(R.id.txtWelcomeUserId)
+    TextView txtWelcomeUserId;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         rLoutStaffView.setOnClickListener(this);
         rLoutFireEvacuationView.setOnClickListener(this);
+
+        txtWelcomeUserId.setText(String.format("Hi , %s", Prefs.getString(PreferenceKeys.USER_NAME, "")));
     }
 
     @Override
